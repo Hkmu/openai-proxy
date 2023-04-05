8f96f65e8cb52a59cc64fb892c8bb4bba4eece47
hj <jiehecd@gmail.com>
2023-04-05 17:14:06 +0800

    Use US regions only

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -2,26 +2,8 @@ import handleRequest from "../../src/handle-request";
 
 export const config = {
   runtime: "edge", // this is a pre-requisite
-  // exclude hongkong cause it's not supported by OpenAI
-  regions: [
-    "cdg1",
-    "arn1",
-    "dub1",
-    "lhr1",
-    "iad1",
-    "sfo1",
-    "pdx1",
-    "cle1",
-    "gru1",
-    "hnd1",
-    "icn1",
-    "kix1",
-    "sin1",
-    "bom1",
-    "syd1",
-    "fra1",
-    "cpt1",
-  ],
+  // https://vercel.com/docs/concepts/edge-network/regions
+  regions: ["cle1", "iad1", "pdx1", "sfo1"],
 };
 
 export default handleRequest;
